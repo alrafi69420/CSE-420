a0a197e939a8e5c9dd9b8539bf9ce14adfc034d5
alrafi69420 <abdullah.al.rafi2@g.bracu.ac.bd>
2026-03-07 23:21:25 +0600

    Update scope_table.h

--- scope_table.h
+++ scope_table.h
@@ -40,16 +40,16 @@ public:
 // complete the methods of scope_table class
 
 
-scope_table::scope_table()
+scope_table::scope_table()                            // Runs when a scope table is created.
 {
     this->bucket_count = 10;
-    this->unique_id = 1;
+    this->unique_id = 1;       //First scope = global scope
     this->table.resize(bucket_count);
 
     outlog << "New ScopeTable with id " << unique_id << " created" << endl << endl;
 }
 
-scope_table::scope_table(int bucket_count, int unique_id, scope_table *parent_scope)
+scope_table::scope_table(int bucket_count, int unique_id, scope_table *parent_scope)        //Used when creating new inner scopes.
 {
     this->bucket_count = bucket_count;
     this->unique_id = unique_id;
@@ -64,16 +64,16 @@ scope_table* scope_table::get_parent_scope()
     return parent_scope;
 }
 
-int scope_table::get_unique_id()
+int scope_table::get_unique_id()                //Returns the scope ID.
 {
     return unique_id;
 }
 
-symbol_info* scope_table::lookup_in_scope(symbol_info* symbol)
+symbol_info* scope_table::lookup_in_scope(symbol_info* symbol)        //Searches for a symbol inside this scope only.
 {
-    int index = hash_function(symbol->getname());
+    int index = hash_function(symbol->getname());                    //Find bucket index.
 
-    for(auto &sym : table[index]) {
+    for(auto &sym : table[index]) {                                   //Go through all symbols stored at this hash index 
         if(sym->getname() == symbol->getname()) {
             return sym;
         }
@@ -84,7 +84,7 @@ symbol_info* scope_table::lookup_in_scope(symbol_info* symbol)
 
 bool scope_table::insert_in_scope(symbol_info* symbol)
 {
-    if(lookup_in_scope(symbol) != NULL) {
+    if(lookup_in_scope(symbol) != NULL) {                    //Check duplicate
         return false;
     }
 
@@ -99,8 +99,8 @@ bool scope_table::delete_from_scope(symbol_info* symbol)
 {
     int index = hash_function(symbol->getname());
 
-    for(auto it = table[index].begin(); it != table[index].end(); ++it) {
-        if((*it)->getname() == symbol->getname()) {
+    for(auto it = table[index].begin(); it != table[index].end(); ++it) {            // Iterate through all the symbols in that bucket
+        if((*it)->getname() == symbol->getname()) {                                    // Checks if current symbol name =  target name
             table[index].erase(it);
             return true;
         }
@@ -113,30 +113,30 @@ bool scope_table::delete_from_scope(symbol_info* symbol)
 
 
 
-void scope_table::print_scope_table(ofstream& outlog)
+void scope_table::print_scope_table(ofstream& outlog)                
 {
-    outlog << "ScopeTable # "+ to_string(unique_id) << endl;
+    outlog << "ScopeTable # "+ to_string(unique_id) << endl;            // Print the ID of the current scope table
 
     //iterate through the current scope table and print the symbols and all relevant information
     for(int i = 0; i < bucket_count; i++) {
         if(table[i].size() > 0) {
-            outlog << i << " --> " << endl;
+            outlog << i << " --> " << endl;            // Print the bucket index
 
             for(auto &symbol : table[i]) {
 
                 outlog << "< " << symbol->getname() 
                        << " : " << symbol->get_type() << " > " << endl;
 
-                if(symbol->get_type_id() == 0) {
+                if(symbol->get_type_id() == 0) {        // If the symbol is var
                     outlog << "Variable" << endl;
                     outlog << "Type: " << symbol->get_data_type() << endl;
                 }
-                else if(symbol->get_type_id() == 1) {
+                else if(symbol->get_type_id() == 1) {        // If the symbol is arr
                     outlog << "Array" << endl;
                     outlog << "Type: " << symbol->get_data_type() << endl;
                     outlog << "Size: " << symbol->get_array_size() << endl;
                 }
-                else {
+                else {                                                    // If the symbol is  func
                     outlog << "Function Definition" << endl;
                     outlog << "Return Type: " << symbol->get_return_type() << endl;
 
@@ -181,3 +181,4 @@ scope_table::~scope_table()
 
     table.clear();
 }
+
